fd6e149f24637a25a5e9c8e5f82e02513b9f1f5c
elbetel90 <elbetel@whyzzer.com>
2025-08-01 16:51:52 +0300

    feat: redis based implementation of simple url shortner service

---
+++ internal/routers/system/system.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"github.com/Elbi123/url-shortner/internal/services"
+	"github.com/labstack/echo/v4"
+)
+
+type SuccessMessage struct {
+	Message string `json:"message"`
+}
+
+type FailureMessage struct {
+	Reason string `json:"reason"`
+}
+
+func NewSystemHandlers(router *echo.Group, app services.URLShortner) {
+	router.GET("/time", GetSystemTime(app))
+}
